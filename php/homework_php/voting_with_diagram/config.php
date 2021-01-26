263ab4634403cd3e555263c2978cf444b2243696
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-01-26 15:39:20 +0300

    chenges php home work

---
+++ php/homework_php/voting_with_diagram/config.php
@@ -0,0 +1,3 @@
+<?php
+$fileName = "voting.txt";
+$separate = " - ";
---
+++ php/homework_php/voting_with_diagram/voting.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+    // print_r($_POST);
+    include "config.php";
+    $data = file($fileName);
+    if (isset($_POST['answer'])) {
+        $currentAnswer = $_POST['answer'];
+        $buff = explode($separate, $data[$currentAnswer + 1]);
+        $buff[1] += 1;
+        $buff[1] .= "\n";
+        $data[$currentAnswer + 1] = $buff[0] . $separate . $buff[1];
+        file_put_contents($fileName, implode("", $data));
+    }
+    $question = $data[0];
+    unset($data[0]);
+
+
+    $answers = [];
+    foreach ($data as $value) {
+        $answers[] = explode($separate, $value);
+    }
+    // print_r($answers);
+    ?>
+
+    <form class="voting_area" action="?" method="POST">
+        <h1><?= $question ?></h1>
+        <br>
+        <?php
+        foreach ($answers as $key => $value) {
+            $count = $value[1] * 1;
+            echo "<input type='radio' value='$key' name='answer'><b>$value[0] ($count)</b><div class='diagram' style='width:$count%'></div><br>\n";
+        }
+        ?>
+        <input type="submit" value="ok">
+    </form>
+</body>
+
+</html>
