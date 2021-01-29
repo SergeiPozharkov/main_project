47db4d89ed462a626aca306d5ec0ad71bb3e62dc
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-01-29 17:04:23 +0300

    chenges php home work

--- php/homework_php/guest_book/guest_book.php
+++ php/homework_php/guest_book/guest_book.php
@@ -37,9 +37,9 @@ if (!empty($_POST)) {
     ?>
     <br>
     <form action="?" method="post">
-         <span><b>Name:</b></span><input type="text" name="name"><br><br>
-         <span><b>Email:</b></span><input type="text" name="email"><br><br>
-         <span><b>Messag:</b></span><br><textarea name="text" id="" cols="30" rows="10"></textarea><br>
+        <span><b>Name:</b></span><input type="text" name="name"><br><br>
+        <span><b>Email:</b></span><input type="text" name="email"><br><br>
+        <span><b>Messag:</b></span><br><textarea name="text" id="" cols="30" rows="10"></textarea><br>
         <input type="submit" value="ok">
     </form>
 </body>
