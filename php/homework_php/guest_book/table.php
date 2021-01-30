d2b913a82d169c1c7bdb3980da8508e3ab5e15df
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-01-30 17:28:49 +0300

    chenges php home work

--- php/homework_php/guest_book/table.php
+++ php/homework_php/guest_book/table.php
@@ -1,14 +1,13 @@
 <?php
-    $data = file_get_contents("guest_data.txt");
-    $records = explode("<======>", $data);
-    echo "<div class=table>";
-    echo "<table border=1>";
-    echo "<th>Name</th><th>Email</th><th>Messag</th>";
-    foreach ($records as $record) {
-        $row = explode("\n", trim($record));
-        // print_r($row);
-        echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
-    }
+$data = file_get_contents("guest_data.txt");
+$records = explode("<======>", $data);
+echo "<div class=table>";
+echo "<table border=1>";
+echo "<th>Name</th><th>Email</th><th>Messag</th><th>Date/Time</th>";
+foreach ($records as $record) {
+    $row = explode("\n", trim($record));
+    echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
+}
 
-    echo "</table>";
-    echo "</div>";
+echo "</table>";
+echo "</div>";
