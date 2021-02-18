6be2db1d10e9e1f8214cfe7774a844bbf1167b18
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-02-18 21:19:16 +0300

    php home work changes

--- php/homework_php/file exchanger/file_exchanger.php
+++ php/homework_php/file exchanger/file_exchanger.php
@@ -17,5 +17,13 @@ foreach ($dir as $value) {
         echo "<a href='files\\$value'><img src='images\xlsx.png' alt='картинка'>$value</a><br><br>";
     } elseif (preg_match($patDocx, $value)) {
         echo "<a href='files\\$value'><img src='images\imgdocx.png' alt='картинка'>$value</a><br><br>";
+    } elseif (exif_imagetype("files\\$value") == IMAGETYPE_GIF) {
+        echo "<a href='files\\$value'><img src='images\imggif.png' alt='картинка'>$value</a><br><br>";
+    } elseif (exif_imagetype("files\\$value") == IMAGETYPE_JPEG) {
+        echo "<a href='files\\$value'><img src='images\imgjpeg.png' alt='картинка'>$value</a><br><br>";
+    } elseif (exif_imagetype("files\\$value") == IMAGETYPE_PNG) {
+        echo "<a href='files\\$value'><img src='images\imgpng.png' alt='картинка'>$value</a><br><br>";
+    } elseif (exif_imagetype("files\\$value") == IMAGETYPE_ICO) {
+        echo "<a href='files\\$value'><img src='images\imgico.png' alt='картинка'>$value</a><br><br>";
     }
 }
