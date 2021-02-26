3bab71c11d8a9c32b0e9e971605a9612e271c942
Sergei <pozharkov.sergei@mail.ru>
2021-02-26 19:30:57 +0300

    changes

--- php/homework_php/guestBook_bootstrap/add.php
+++ php/homework_php/guestBook_bootstrap/add.php
@@ -6,7 +6,7 @@ include "functions.php";
 
 $data = json_decode(file_get_contents("data.json"));
 
-$data[] = ['msg' => $_POST["msg"], 'name' => $_POST["name"], 'time' => time()];
+$data[] = ['name' => $_POST["name"], 'msg' => $_POST["msg"], 'time' => time()];
 
 if (!censure($_POST["msg"]) && !empty($_POST["msg"]) && !empty($_POST["name"])) {
     file_put_contents('data.json', json_encode($data));
