3c5114ef063fdbbaf0e3e40ff5379a76a067c678
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-04-01 20:33:47 +0300

    added login form for admin and bootstrap

---
+++ php/homework_php/survey_with_crud/include/include_footer.php
@@ -0,0 +1,9 @@
+<!-- Файл для подключения bootstrap стилей к алертам (нижняя часть html страницы) -->
+</div>
+<div class="col-sm">
+
+</div>
+</div>
+</div>
+</body>
+</html>
--- php/homework_php/survey_with_crud/login_for_admin.php
+++ php/homework_php/survey_with_crud/login_for_admin.php
@@ -7,23 +7,27 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
-<body class="thanks">
+<body>
 <div class="container">
     <div class="row">
         <div class="col-sm">
-            <div class="alert alert-success" role="alert">
-                <h1>Огромное спасибо за прохождение опроса!</h1>
-            </div>
+
+        </div>
+        <div class="col-sm">
+            <h1>Форма авторизации</h1>
+            <form action="login_check.php" method="post">
+                <input type="text" placeholder="Введите свой логин" name="login" id="login" class="form-control"><br>
+                <input type="password" placeholder="Введите свой пароль" name="pass" id="pass" class="form-control"><br>
+                <button type="submit" class="btn btn-primary">Войти</button>
+            </form>
+        </div>
+        <div class="col-sm">
+
         </div>
     </div>
 </div>
-<a id="retry" class="btn btn-primary" href="opros.php">Пройти опрос заново</a>
-<br>
-<a class="btn btn-primary" href="login_for_admin.php">Только для администратора</a>
-
 </body>
 </html>
-<?php
+
