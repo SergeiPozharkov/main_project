b6f80c1c042a6c27085e47ac92a2d294740457bc
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-04-02 17:41:03 +0300

    added dump db

---
+++ php/homework_php/survey_with_crud/dump/questionnaire.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 02, 2021 at 05:38 PM
+-- Server version: 8.0.19
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `questionnaire`
+--
+CREATE DATABASE IF NOT EXISTS `questionnaire` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `questionnaire`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers`
+--
+
+CREATE TABLE `answers` (
+  `id` int NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `age` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `answers`
+--
+
+INSERT INTO `answers` (`id`, `city`, `email`, `gender`, `age`) VALUES
+(1, 'Витебск', 'pozharkov.sergei@mail.ru', 'Мужчина', 23),
+(2, 'Витебск', 'edikus12345@gmail.com', 'Мужчина', 25),
+(3, 'Витебск', 'edikus12345@gmail.com', 'Мужчина', 56),
+(4, 'Витебск', 'pozharkov.sergei@mail.ru', 'Мужчина', 23),
+(5, 'Витебск', 'Kesha_huligan1234@mail.ru', 'Мужчина', 55),
+(6, 'Гродно', 'pozharkov.sergei@mail.ru', 'Женщина', 60),
+(8, 'Минск', 'vaska.pupkin@mail.ru', 'Мужчина', 13);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
