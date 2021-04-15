0a250388bbbc8267e4a6cc849b9798603b1f62e1
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-04-16 01:30:12 +0300

    php home work changes

---
+++ php/homework_php/calc_table/calculation.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body class="calculation">
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+        </div>
+        <div class="col-sm">
+            <?php
+
+            include "class/Table.php";
+
+            $calc = new Table($_POST["tableTop"], $_POST["tableLegs"], $_POST["jobCost"]);
+
+            echo "<div class='alert alert-success' id='calculation__alert' role='alert'>Стоимость =  
+            {$calc->{$_POST["converter"]}()} $ (в т.ч. стоимость работы {$_POST['jobCost']} $)</div>";
+            echo "<a href='index.html' class='btn btn-primary'>Вернуться</a>";
+            ?>
+        </div>
+        <div class="col-sm">
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ php/homework_php/calc_table/class/Table.php
@@ -0,0 +1,68 @@
+<?php
+
+
+class Table
+{
+    public float $jobCost;
+    public float $tableTop;
+    public float $tableLegs;
+
+    public function __construct($tableTop, $tableLegs, $jobCost)
+    {
+        $this->setJobCost($jobCost);
+        $this->setTableTopDm($tableTop);
+        $this->setTableLegsDm($tableLegs);
+
+    }
+
+    /** Высота ножек в дм.
+     * @param float $tableLegs
+     */
+    public function setTableLegsDm(float $tableLegs): void
+    {
+        if ($tableLegs > 0) {
+            $this->tableLegs = $tableLegs;
+        }
+    }
+
+
+    /** Площадь столешницы в дм.
+     * @param float $tableTop
+     */
+    public function setTableTopDm(float $tableTop): void
+    {
+
+        if ($tableTop > 0) {
+            $this->tableTop = $tableTop;
+        }
+
+    }
+
+    /** Стоимость работы.
+     * @param float $jobCost
+     */
+    public function setJobCost(float $jobCost): void
+    {
+        $this->jobCost = $jobCost;
+    }
+
+    public function tableDm(): float
+    {
+        return $this->tableTop + $this->tableLegs + $this->jobCost;
+    }
+
+    public function tableSm(): float|int
+    {
+        return $this->tableTop / 100 + $this->tableLegs / 10 + $this->jobCost;
+    }
+
+    public function tableM(): float|int
+    {
+        return $this->tableTop * 100 + $this->tableLegs * 10 + $this->jobCost;
+    }
+
+    public function tableInch(): float
+    {
+        return $this->tableTop / 15.5 + $this->tableLegs / 3.93700787402 + $this->jobCost;
+    }
+}
