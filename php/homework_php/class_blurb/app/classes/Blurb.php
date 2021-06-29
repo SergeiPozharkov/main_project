18e19c7fc2a42738a6794322d7c5bca9004804f9
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-06-29 18:22:07 +0300

    changes files Blurb, index and script

--- php/homework_php/class_blurb/app/classes/Blurb.php
+++ php/homework_php/class_blurb/app/classes/Blurb.php
@@ -16,11 +16,11 @@ class Blurb
     public function blurbGeneration(): void
     {
         echo "Продается машина марки {$this->iCar->name()}, имеющая следующие параметры:<br><ul>
-        <li>Цвет -{$this->iCar->color()}</li>
-        <li>Максимальная скорость -{$this->iCar->maxSpeed()} км/ч</li>
-        <li>Расход топлива -{$this->iCar->consumptionFuel()} л.</li>
-        <li>Объем двигателя -{$this->iCar->engineVolume()} л.</li>
-        </ul><br> по цене {$this->iCar->price()}бел.руб. (торг уместен)";
+        <li>Цвет - {$this->iCar->color()}</li>
+        <li>Максимальная скорость - {$this->iCar->maxSpeed()} км/ч</li>
+        <li>Расход топлива - {$this->iCar->consumptionFuel()} л.</li>
+        <li>Объем двигателя - {$this->iCar->engineVolume()} л.</li>
+        </ul> по цене {$this->iCar->price()}  бел.руб. (торг уместен)";
     }
 
 }
--- php/homework_php/class_blurb/public/index.php
+++ php/homework_php/class_blurb/public/index.php
@@ -5,18 +5,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
 <h1>Генерация рекламного объявления для продажи машины</h1>
 <form action="script.php" method="post">
-    <span>Марка автомобиля </span><input type="text" name="name"><br><br>
-    <span>Цвет автомобиля </span><input type="text" name="color"><br><br>
-    <span>Цена автомобиля </span><input type="text" name="price"><br><br>
-    <span>Максимальная скорость автомобиля </span><input type="text" name="max_speed"><br><br>
-    <span>Мощность двигателя автомобиля </span><input type="text" name="engine_volume"><br><br>
-    <span>Расход топлива автомобиля </span><input type="text" name="fuel_consumption"><br><br>
-    <input type="submit" value="Сгенерировать">
+    <span>Марка автомобиля </span><br><input type="text" name="name"><br><br>
+    <span>Цвет автомобиля </span><br><input type="text" name="color"><br><br>
+    <span>Цена автомобиля </span><br><input type="text" name="price"><br><br>
+    <span>Максимальная скорость автомобиля </span><br><input type="text" name="max_speed"><br><br>
+    <span>Мощность двигателя автомобиля </span><br><input type="text" name="engine_volume"><br><br>
+    <span>Расход топлива автомобиля </span><br><input type="text" name="fuel_consumption"><br><br>
+    <input type="submit" class="btn btn-primary" value="Сгенерировать">
 </form>
 </body>
 </html>
